e27f55d80a35f89503cda277a33eca4f19df6069
RussellCaletena <RussellCaletena@gmail.com>
2017-06-13 00:50:09 -0700

    7 needs work

---
+++ listPractice/7.py
@@ -0,0 +1,12 @@
+distances = [('San Luis Obispo', 'Atascadero', 16.8),
+             ('Atascadero', 'San Francisco', 214.6)]
+
+route1 = ['Atascadero', 'San Luis Obispo', 'Atascadero', 'San Francisco', 'Atascadero']
+
+def distance_two_cities(listOfRoutes, listOfDistances):
+    distance = 0
+
+    print (distance)
+
+
+distance_two_cities(route1)
