6bfd56d08bf6a0a168695da0b198cb768709de8c
RussellCaletena <RussellCaletena@gmail.com>
2017-06-13 14:46:49 -0700

    2 works

--- listPractice/2.py
+++ listPractice/2.py
@@ -1,15 +1,21 @@
 lines = ['Hello there.',
          'How now brown cow?',
          'I like cheese']
+
 def line_counts(list1, character):
     newList = []
-    count = 0
     i = 0
     while i < len(list1):
-        if character in list1[i]:
-            count += 1
-            newList.append(count)
+        number = characterCount(list1[i], character)
+        newList.append(number)
         i += 1
     print (newList)
+
+def characterCount(string ,character):
+    charCount = 0
+    for i in range(len(string)):
+        if string[i] == character:
+            charCount += 1
+    return charCount
 
 line_counts(lines, 'o')
